ee06e88e13cc8c31fcdb75f7cdbb0e6c9ed69e3e
安牛 <zhenhua.xu@kongapi.com>
2019-07-30 15:48:05 +0800

    消息中心 修改

--- public/IM/im/js/link.js
+++ public/IM/im/js/link.js
@@ -20,10 +20,10 @@ var SDKBridge = function(ctr, data) {
   this.cache = data;
   window.nim = ctr.nim = this.nim = SDK.NIM.getInstance({
     //控制台日志，上线时应该关掉
-    // debug: true || {
-    //   api: 'info',
-    //   style: 'font-size:14px;color:blue;background-color:rgba(0,0,0,0.1)'
-    // },
+    debug: true || {
+      api: 'info',
+      style: 'font-size:14px;color:blue;background-color:rgba(0,0,0,0.1)'
+    },
     db: true,
     appKey: CONFIG.appkey,
     account: userUID,
