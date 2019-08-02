56e87256a6666d33bdda7aff55b5735e8bf13b10
兴 湛 <869070197@qq.com>
2019-08-02 14:15:47 +0800

    no message

--- src/views/merchant/register-h5.vue
+++ src/views/merchant/register-h5.vue
@@ -149,6 +149,7 @@ export default {
         let phone = this.phone
         if(!(/^(0|86|17951)?(13[0-9]|15[012356789]|166|17[3678]|18[0-9]|14[57])[0-9]{8}$/.test(phone))){
             Toast('请输入正确的手机号')
+            this.isCode = true
             return
         }
         this.captchaIns && this.captchaIns.verify()
