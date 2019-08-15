a81ab3945f16b5053f89304ca321e4f0e43b281c
兴 湛 <869070197@qq.com>
2019-08-15 19:06:03 +0800

    no message

--- src/views/merchantPC/mainBusiness.vue
+++ src/views/merchantPC/mainBusiness.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="mainBusinesss">
-    <div class="business">
+    <div class="business" v-show="!noData">
       <div class="business_title">
         <h4>主营业务管理</h4>
         <span class="addBusiness" @click="handleFetchPv">+添加新业务</span>
@@ -224,6 +224,26 @@
         </el-dialog>
       </div>
     </div>
+    <div class="business" v-show="noData">
+      <div class="business_title">
+        <h4>主营业务批量添加</h4>
+        <!-- <span class="addBusiness" @click="handleFetchPv">+添加新业务</span> -->
+      </div>
+      <div class="business_content_noData">
+        <div class="case">
+          <div class="title">
+            <span>一级服务</span>
+            <span>二级服务</span>
+            <span>三级服务</span>
+          </div>
+          <el-cascader-panel v-model="casList" :props="props" :options="options" @change="casa"></el-cascader-panel>
+        </div>
+        <span slot="footer" class="dialog-footer" style="display: flex;justify-content: flex-end;width:100%;margin-top: 16PX;">
+          <!-- <el-button @click="dialogRefuseVisible = false">返回</el-button> -->
+          <el-button style="width: 120PX;" type="primary" @click="refuse">保存</el-button>
+        </span>
+      </div>
+    </div>
   </div>
 </template>
 <script>
@@ -252,6 +272,7 @@ export default {
   },
   data(){
     return {
+      noData: true,
       listQuery: {
         pageNum: 1,
         pageSize: 20,
@@ -298,7 +319,208 @@ export default {
         id: '',
         introduceContent: {},
         introduceType: ''
-      }
+      },
+      //批量上架数据
+      props: {
+        multiple: true
+      },
+      casList: [],
+      oneList: [],
+      options: [{
+        value: 'zhinan',
+        label: '指南',
+        children: [{
+          value: 'shejiyuanze',
+          label: '设计原则',
+          children: [{
+            value: 'yizhi',
+            label: '一致'
+          }, {
+            value: 'fankui',
+            label: '反馈'
+          }, {
+            value: 'xiaolv',
+            label: '效率'
+          }, {
+            value: 'kekong',
+            label: '可控'
+          }]
+        }, {
+          value: 'daohang',
+          label: '导航',
+          children: [{
+            value: 'cexiangdaohang',
+            label: '侧向导航'
+          }, {
+            value: 'dingbudaohang',
+            label: '顶部导航'
+          }]
+        }]
+      }, {
+        value: 'zujian',
+        label: '组件',
+        children: [{
+          value: 'basic',
+          label: 'Basic',
+          children: [{
+            value: 'layout',
+            label: 'Layout 布局'
+          }, {
+            value: 'color',
+            label: 'Color 色彩'
+          }, {
+            value: 'typography',
+            label: 'Typography 字体'
+          }, {
+            value: 'icon',
+            label: 'Icon 图标'
+          }, {
+            value: 'button',
+            label: 'Button 按钮'
+          }]
+        }, {
+          value: 'form',
+          label: 'Form',
+          children: [{
+            value: 'radio',
+            label: 'Radio 单选框'
+          }, {
+            value: 'checkbox',
+            label: 'Checkbox 多选框'
+          }, {
+            value: 'input',
+            label: 'Input 输入框'
+          }, {
+            value: 'input-number',
+            label: 'InputNumber 计数器'
+          }, {
+            value: 'select',
+            label: 'Select 选择器'
+          }, {
+            value: 'cascader',
+            label: 'Cascader 级联选择器'
+          }, {
+            value: 'switch',
+            label: 'Switch 开关'
+          }, {
+            value: 'slider',
+            label: 'Slider 滑块'
+          }, {
+            value: 'time-picker',
+            label: 'TimePicker 时间选择器'
+          }, {
+            value: 'date-picker',
+            label: 'DatePicker 日期选择器'
+          }, {
+            value: 'datetime-picker',
+            label: 'DateTimePicker 日期时间选择器'
+          }, {
+            value: 'upload',
+            label: 'Upload 上传'
+          }, {
+            value: 'rate',
+            label: 'Rate 评分'
+          }, {
+            value: 'form',
+            label: 'Form 表单'
+          }]
+        }, {
+          value: 'data',
+          label: 'Data',
+          children: [{
+            value: 'table',
+            label: 'Table 表格'
+          }, {
+            value: 'tag',
+            label: 'Tag 标签'
+          }, {
+            value: 'progress',
+            label: 'Progress 进度条'
+          }, {
+            value: 'tree',
+            label: 'Tree 树形控件'
+          }, {
+            value: 'pagination',
+            label: 'Pagination 分页'
+          }, {
+            value: 'badge',
+            label: 'Badge 标记'
+          }]
+        }, {
+          value: 'notice',
+          label: 'Notice',
+          children: [{
+            value: 'alert',
+            label: 'Alert 警告'
+          }, {
+            value: 'loading',
+            label: 'Loading 加载'
+          }, {
+            value: 'message',
+            label: 'Message 消息提示'
+          }, {
+            value: 'message-box',
+            label: 'MessageBox 弹框'
+          }, {
+            value: 'notification',
+            label: 'Notification 通知'
+          }]
+        }, {
+          value: 'navigation',
+          label: 'Navigation',
+          children: [{
+            value: 'menu',
+            label: 'NavMenu 导航菜单'
+          }, {
+            value: 'tabs',
+            label: 'Tabs 标签页'
+          }, {
+            value: 'breadcrumb',
+            label: 'Breadcrumb 面包屑'
+          }, {
+            value: 'dropdown',
+            label: 'Dropdown 下拉菜单'
+          }, {
+            value: 'steps',
+            label: 'Steps 步骤条'
+          }]
+        }, {
+          value: 'others',
+          label: 'Others',
+          children: [{
+            value: 'dialog',
+            label: 'Dialog 对话框'
+          }, {
+            value: 'tooltip',
+            label: 'Tooltip 文字提示'
+          }, {
+            value: 'popover',
+            label: 'Popover 弹出框'
+          }, {
+            value: 'card',
+            label: 'Card 卡片'
+          }, {
+            value: 'carousel',
+            label: 'Carousel 走马灯'
+          }, {
+            value: 'collapse',
+            label: 'Collapse 折叠面板'
+          }]
+        }]
+      }, {
+        value: 'ziyuan',
+        label: '资源',
+        children: [{
+          value: 'axure',
+          label: 'Axure Components'
+        }, {
+          value: 'sketch',
+          label: 'Sketch Templates'
+        }, {
+          value: 'jiaohu',
+          label: '组件交互文档'
+        }]
+      }],
     }
   },
   created(){
@@ -1012,7 +1234,29 @@ export default {
           }
         })
       })
-    }
+    },
+    casa(e){
+      // console.log(e)
+      console.log(e)
+      let oneList = []
+      for (let i = 0; i < this.casList.length; i++) {
+        if(oneList.indexOf(this.casList[i][0]) == -1){
+          oneList.push(this.casList[i][0])
+          this.oneList = oneList
+          console.log(this.oneList)
+        }
+      }
+      if(this.oneList.length > 3){
+        this.$message({
+          message: '最多三个',
+          type: 'error',
+          showClose: true,
+          duration: 1000
+        })
+        return
+      }
+    },
+    refuse(){}
   }
 }
 </script>
@@ -1187,6 +1431,49 @@ export default {
             }
           }
         }  
+      }
+    }
+    .business_content_noData{
+      width: 93.84%;
+      margin-left: auto;
+      margin-right: auto;
+      display: flex;
+      align-items: flex-start;
+      // justify-content: space-between;
+      flex-flow: column;
+      margin-top: 24PX;
+      .case{
+        width: 100%;
+        display: flex;
+        flex-flow: column;
+        align-items: center;
+        .title{
+          display: flex;
+          align-items: center;
+          width: 100%;
+          justify-content: space-between;
+          span{
+            display: block;
+            width: 33.33%;
+            height: 40PX;
+            line-height: 40PX;
+            text-align: center;
+            background: linear-gradient(135deg, #FF7F4A 0%, #FB5332 100%);
+            font-size: 14PX;
+            color: #ffffff;
+          }
+        }
+        .el-cascader-panel.is-bordered{
+          width: 100%;
+          display: flex;
+          // justify-content: space-between;
+          .el-cascader-menu{
+            width: 33.33%;
+          }
+          .el-cascader-menu__wrap{
+            height: 400PX;
+          }
+        }
       }
     }
   }
